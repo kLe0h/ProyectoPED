f77aeec10128ac809e8aaf87af2501b753aa603d
kLe0h <73725512+kLe0h@users.noreply.github.com>
2022-05-06 20:09:21 -0600

    agregado login

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -29,106 +29,232 @@ namespace ProyectoPED
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.txtContrasenia = new System.Windows.Forms.TextBox();
-            this.btnIngresar = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.btnIrARegistrar = new System.Windows.Forms.Button();
-            this.btnSalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btn_ingresar = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(58, 96);
+            this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(537, 153);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 25);
+            this.label1.Size = new System.Drawing.Size(83, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(58, 165);
+            this.label2.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(537, 211);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 25);
+            this.label2.Size = new System.Drawing.Size(117, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Contraseña:";
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(189, 99);
+            this.txtUsuario.Location = new System.Drawing.Point(660, 157);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(2);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(197, 22);
+            this.txtUsuario.Size = new System.Drawing.Size(149, 20);
             this.txtUsuario.TabIndex = 2;
             // 
             // txtContrasenia
             // 
-            this.txtContrasenia.Location = new System.Drawing.Point(189, 168);
+            this.txtContrasenia.Location = new System.Drawing.Point(660, 215);
+            this.txtContrasenia.Margin = new System.Windows.Forms.Padding(2);
             this.txtContrasenia.Name = "txtContrasenia";
-            this.txtContrasenia.Size = new System.Drawing.Size(197, 22);
+            this.txtContrasenia.Size = new System.Drawing.Size(149, 20);
             this.txtContrasenia.TabIndex = 3;
             this.txtContrasenia.UseSystemPasswordChar = true;
-            // 
-            // btnIngresar
-            // 
-            this.btnIngresar.Location = new System.Drawing.Point(298, 210);
-            this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(88, 32);
-            this.btnIngresar.TabIndex = 4;
-            this.btnIngresar.Text = "Ingresar";
-            this.btnIngresar.UseVisualStyleBackColor = true;
-            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(97, 20);
+            this.label3.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(258, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(248, 46);
+            this.label3.Size = new System.Drawing.Size(303, 27);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Bienvenidos";
+            this.label3.Text = "¡Bienvenido a PastriesLand!";
             // 
-            // btnIrARegistrar
+            // pictureBox1
             // 
-            this.btnIrARegistrar.Location = new System.Drawing.Point(204, 210);
-            this.btnIrARegistrar.Name = "btnIrARegistrar";
-            this.btnIrARegistrar.Size = new System.Drawing.Size(88, 32);
-            this.btnIrARegistrar.TabIndex = 6;
-            this.btnIrARegistrar.Text = "Registrar";
-            this.btnIrARegistrar.UseVisualStyleBackColor = true;
-            this.btnIrARegistrar.Click += new System.EventHandler(this.btnIrARegistrar_Click);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(57, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(420, 332);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
             // 
-            // btnSalir
+            // label4
             // 
-            this.btnSalir.Location = new System.Drawing.Point(298, 248);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(88, 32);
-            this.btnSalir.TabIndex = 7;
-            this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
-            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(565, 86);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 27);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Inicia sesión aquí";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(553, 395);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(219, 23);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "¿No tienes una cuenta?";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Brown;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(10)))), ((int)(((byte)(68)))));
+            this.linkLabel1.Location = new System.Drawing.Point(595, 433);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(132, 19);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "¡Registrate aquí!";
+            this.linkLabel1.Click += new System.EventHandler(this.btnIrARegistrar_Click);
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.Activecolor = System.Drawing.Color.Firebrick;
+            this.btn_ingresar.BackColor = System.Drawing.Color.Firebrick;
+            this.btn_ingresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_ingresar.BorderRadius = 0;
+            this.btn_ingresar.ButtonText = "Ingresar";
+            this.btn_ingresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ingresar.DisabledColor = System.Drawing.Color.Gray;
+            this.btn_ingresar.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ingresar.Iconcolor = System.Drawing.Color.Transparent;
+            this.btn_ingresar.Iconimage = null;
+            this.btn_ingresar.Iconimage_right = null;
+            this.btn_ingresar.Iconimage_right_Selected = null;
+            this.btn_ingresar.Iconimage_Selected = null;
+            this.btn_ingresar.IconMarginLeft = 0;
+            this.btn_ingresar.IconMarginRight = 0;
+            this.btn_ingresar.IconRightVisible = true;
+            this.btn_ingresar.IconRightZoom = 0D;
+            this.btn_ingresar.IconVisible = true;
+            this.btn_ingresar.IconZoom = 90D;
+            this.btn_ingresar.IsTab = false;
+            this.btn_ingresar.Location = new System.Drawing.Point(582, 275);
+            this.btn_ingresar.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Normalcolor = System.Drawing.Color.Firebrick;
+            this.btn_ingresar.OnHovercolor = System.Drawing.Color.Firebrick;
+            this.btn_ingresar.OnHoverTextColor = System.Drawing.Color.Maroon;
+            this.btn_ingresar.selected = false;
+            this.btn_ingresar.Size = new System.Drawing.Size(157, 43);
+            this.btn_ingresar.TabIndex = 14;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_ingresar.Textcolor = System.Drawing.Color.White;
+            this.btn_ingresar.TextFont = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ingresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(770, 19);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(39, 31);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 24;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // bunifuFlatButton1
+            // 
+            this.bunifuFlatButton1.Activecolor = System.Drawing.Color.LightCoral;
+            this.bunifuFlatButton1.BackColor = System.Drawing.Color.RosyBrown;
+            this.bunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.bunifuFlatButton1.BorderRadius = 0;
+            this.bunifuFlatButton1.ButtonText = "Salir";
+            this.bunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuFlatButton1.DisabledColor = System.Drawing.Color.Gray;
+            this.bunifuFlatButton1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent;
+            this.bunifuFlatButton1.Iconimage = null;
+            this.bunifuFlatButton1.Iconimage_right = null;
+            this.bunifuFlatButton1.Iconimage_right_Selected = null;
+            this.bunifuFlatButton1.Iconimage_Selected = null;
+            this.bunifuFlatButton1.IconMarginLeft = 0;
+            this.bunifuFlatButton1.IconMarginRight = 0;
+            this.bunifuFlatButton1.IconRightVisible = true;
+            this.bunifuFlatButton1.IconRightZoom = 0D;
+            this.bunifuFlatButton1.IconVisible = true;
+            this.bunifuFlatButton1.IconZoom = 90D;
+            this.bunifuFlatButton1.IsTab = false;
+            this.bunifuFlatButton1.Location = new System.Drawing.Point(609, 328);
+            this.bunifuFlatButton1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.bunifuFlatButton1.Name = "bunifuFlatButton1";
+            this.bunifuFlatButton1.Normalcolor = System.Drawing.Color.LightCoral;
+            this.bunifuFlatButton1.OnHovercolor = System.Drawing.Color.LightCoral;
+            this.bunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.Maroon;
+            this.bunifuFlatButton1.selected = false;
+            this.bunifuFlatButton1.Size = new System.Drawing.Size(108, 38);
+            this.bunifuFlatButton1.TabIndex = 25;
+            this.bunifuFlatButton1.Text = "Salir";
+            this.bunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuFlatButton1.Textcolor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.TextFont = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuFlatButton1.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 314);
-            this.Controls.Add(this.btnSalir);
-            this.Controls.Add(this.btnIrARegistrar);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(137)))), ((int)(((byte)(158)))));
+            this.ClientSize = new System.Drawing.Size(836, 501);
+            this.Controls.Add(this.bunifuFlatButton1);
+            this.Controls.Add(this.bunifuImageButton1);
+            this.Controls.Add(this.btn_ingresar);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.txtContrasenia);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Iniciar seción";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,9 +266,13 @@ namespace ProyectoPED
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.TextBox txtContrasenia;
-        private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnIrARegistrar;
-        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private Bunifu.Framework.UI.BunifuFlatButton btn_ingresar;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
     }
 }
--- Modelo.cs
+++ Modelo.cs
@@ -28,8 +28,8 @@ namespace ProyectoPED
             
         }
 
-        
 
+        
         public bool existeUsuario(string usuario)
         {
             MySqlDataReader reader;
